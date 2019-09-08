5e8d0422d446ba7a2a7b6145bb1fae03de18d2bb
Johnny Ramirez <JRProgramming@users.noreply.github.com>
2019-09-08 14:22:42 -0400

    Adds planner functionality

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+window.onload = function() {
+    for(var i=0;i<document.getElementsByClassName("homework").length; i++) {
+        document.getElementsByClassName("homework")[i].innerHTML = localStorage.getItem(i)
+        checkBoxes(i)
+    }
+}
 function identifyTextField(text) {
     document.getElementsByClassName("textField")[text].addEventListener("keyup", function(event) {
          if (event.keyCode === 13) {
@@ -9,10 +15,55 @@ function createCheckBox(text) {
     if(document.getElementsByClassName("textField")[text].value != "") {
         var checkbox = document.createElement("input")
         checkbox.setAttribute("type", "checkbox")
+        var id = document.getElementsByClassName("cb" + text).length
+        checkbox.setAttribute("class", "cb" + text)
+        checkbox.setAttribute("onclick", "checkBoxes(" + text + "," + id  + ")")
         var label = document.createElement("label")
         label.innerHTML = document.getElementsByClassName("textField")[text].value + "<br>"
         document.getElementsByClassName("homework")[text].appendChild(checkbox)
         document.getElementsByClassName("homework")[text].appendChild(label)
         document.getElementsByClassName("textField")[text].value = "";
     }
+    localStorage.setItem(text, document.getElementsByClassName("homework")[text].innerHTML)
+}
+function checkBoxes(box, id) {
+    var allBoxesChecked = true
+    var checkBoxes = document.getElementsByClassName("cb" + box)
+    if(checkBoxes.length != 0) {
+        for(i=0;i<checkBoxes.length; i++) {
+            if(!checkBoxes[i].checked) {
+                allBoxesChecked = false
+            }
+        }
+    } else {
+        allBoxesChecked = false
+    }
+    if(allBoxesChecked) {
+        var button = document.createElement("button")
+        button.innerHTML = "Remove"
+        button.setAttribute("style", "color: grey; background-color: lightgrey; border: solid lightgrey; display: inline; float: right;")
+        button.setAttribute("onclick", "removeHomework(" + box + ")")
+        document.getElementsByTagName("h3")[box].appendChild(button)
+    } else if(document.getElementsByTagName("h3")[box].contains(document.getElementsByTagName("button")[0])) {
+        document.getElementsByTagName("h3")[box].removeChild(document.getElementsByTagName("button")[0])
+    }
+    saveCheckBox(box, id)
+}
+function saveCheckBox(box, id) {
+    if(id != null) {
+       if(document.getElementsByClassName("cb" + box)[id].checked) {
+           document.getElementsByClassName("cb" + box)[id].setAttribute("checked", true)
+       } else {
+           document.getElementsByClassName("cb" + box)[id].removeAttribute("checked")
+       }
+       localStorage.setItem(box, document.getElementsByClassName("homework")[box].innerHTML)
+    }
+}
+function removeHomework(box) {
+    var homework = document.getElementsByClassName("homework")[box]
+    while(homework.hasChildNodes()) {
+        homework.removeChild(homework.firstChild)
+    }
+    document.getElementsByTagName("h3")[box].removeChild(document.getElementsByTagName("h3")[box].lastChild)
+    localStorage.setItem(box, document.getElementsByClassName("homework")[box].innerHTML)
 }
