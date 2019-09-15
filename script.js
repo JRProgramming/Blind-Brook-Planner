5ce5b45079be5bc87d888e055f567a1d7ff9b769
JRProgramming <ramirezjohnny@icloud.com>
2019-09-15 11:07:32 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,167 @@
-var _0x880b=["\x6F\x6E\x6C\x6F\x61\x64","\x63\x6C\x61\x73\x73","\x67\x65\x74\x49\x74\x65\x6D","\x69\x6E\x6E\x65\x72\x48\x54\x4D\x4C","\x6D\x61\x69\x6E","\x67\x65\x74\x45\x6C\x65\x6D\x65\x6E\x74\x73\x42\x79\x54\x61\x67\x4E\x61\x6D\x65","\x74\x69\x74\x6C\x65","\x64\x6F\x63\x75\x6D\x65\x6E\x74","\x6E\x61\x6D\x65","\x27\x73\x20\x50\x6C\x61\x6E\x6E\x65\x72","\x68\x65\x61\x64\x69\x6E\x67","\x67\x65\x74\x45\x6C\x65\x6D\x65\x6E\x74\x42\x79\x49\x64","\x6C\x65\x6E\x67\x74\x68","\x68\x6F\x6D\x65\x77\x6F\x72\x6B","\x67\x65\x74\x45\x6C\x65\x6D\x65\x6E\x74\x73\x42\x79\x43\x6C\x61\x73\x73\x4E\x61\x6D\x65","\x76\x69\x73\x69\x62\x69\x6C\x69\x74\x79","\x73\x74\x79\x6C\x65","\x78\x2D\x70\x6F\x70\x75\x70","\x68\x69\x64\x64\x65\x6E","\x6B\x65\x79\x75\x70","\x6B\x65\x79\x43\x6F\x64\x65","\x61\x64\x64\x45\x76\x65\x6E\x74\x4C\x69\x73\x74\x65\x6E\x65\x72","\x74\x65\x78\x74\x46\x69\x65\x6C\x64","\x69\x64","\x76\x61\x6C\x75\x65","","\x69\x6E\x70\x75\x74","\x63\x72\x65\x61\x74\x65\x45\x6C\x65\x6D\x65\x6E\x74","\x74\x79\x70\x65","\x63\x68\x65\x63\x6B\x62\x6F\x78","\x73\x65\x74\x41\x74\x74\x72\x69\x62\x75\x74\x65","\x63\x62","\x6F\x6E\x63\x6C\x69\x63\x6B","\x63\x68\x65\x63\x6B\x42\x6F\x78\x65\x73\x28","\x2C","\x29","\x6C\x61\x62\x65\x6C","\x6C","\x3C\x69\x6E\x70\x75\x74\x20\x74\x79\x70\x65\x3D\x22\x74\x65\x78\x74\x22\x20\x63\x6C\x61\x73\x73\x3D\x22\x68\x6F\x6D\x65\x77\x6F\x72\x6B\x4C\x61\x62\x65\x6C\x22\x20\x69\x64\x3D\x27\x69","\x27\x20\x73\x69\x7A\x65\x3D","\x20\x76\x61\x6C\x75\x65\x3D\x27","\x27\x3E\x3C\x62\x72\x3E","\x61\x70\x70\x65\x6E\x64\x43\x68\x69\x6C\x64","\x6F\x6E\x6B\x65\x79\x75\x70","\x72\x65\x6D\x6F\x76\x65\x41\x73\x73\x69\x67\x6E\x6D\x65\x6E\x74\x28","\x69","\x73\x65\x74\x49\x74\x65\x6D","\x63\x68\x65\x63\x6B\x65\x64","\x62","\x62\x75\x74\x74\x6F\x6E","\x52\x65\x6D\x6F\x76\x65","\x63\x6F\x6C\x6F\x72\x3A\x20\x67\x72\x65\x79\x3B\x20\x62\x61\x63\x6B\x67\x72\x6F\x75\x6E\x64\x2D\x63\x6F\x6C\x6F\x72\x3A\x20\x6C\x69\x67\x68\x74\x67\x72\x65\x79\x3B\x20\x62\x6F\x72\x64\x65\x72\x3A\x20\x73\x6F\x6C\x69\x64\x20\x6C\x69\x67\x68\x74\x67\x72\x65\x79\x3B\x20\x64\x69\x73\x70\x6C\x61\x79\x3A\x20\x69\x6E\x6C\x69\x6E\x65\x3B\x20\x66\x6C\x6F\x61\x74\x3A\x20\x72\x69\x67\x68\x74\x3B","\x72\x65\x6D\x6F\x76\x65\x48\x6F\x6D\x65\x77\x6F\x72\x6B\x28","\x68\x33","\x72\x65\x6D\x6F\x76\x65\x43\x68\x69\x6C\x64","\x72\x65\x6D\x6F\x76\x65\x41\x74\x74\x72\x69\x62\x75\x74\x65","\x66\x69\x72\x73\x74\x43\x68\x69\x6C\x64","\x68\x61\x73\x43\x68\x69\x6C\x64\x4E\x6F\x64\x65\x73","\x73\x69\x7A\x65","\x76\x69\x73\x69\x62\x6C\x65","\x70\x6F\x70\x75\x70","\x61\x75\x74\x6F\x66\x6F\x63\x75\x73","\x63\x6C\x61\x73\x73\x54\x65\x78\x74","\x6E\x61\x6D\x65\x2D\x70\x6F\x70\x75\x70","\x6E\x61\x6D\x65\x2D\x54\x65\x78\x74","\x70\x6F\x70\x75\x70\x2D\x62\x61\x63\x6B\x67\x72\x6F\x75\x6E\x64","\x64\x69\x76","\x74\x65\x78\x74","\x70\x6C\x61\x63\x65\x68\x6F\x6C\x64\x65\x72","\x4E\x65\x77\x20\x68\x6F\x6D\x65\x77\x6F\x72\x6B","\x69\x64\x65\x6E\x74\x69\x66\x79\x54\x65\x78\x74\x46\x69\x65\x6C\x64\x28"];window[_0x880b[0]]= function(){if(localStorage[_0x880b[2]](_0x880b[1])){document[_0x880b[5]](_0x880b[4])[0][_0x880b[3]]= localStorage[_0x880b[2]](_0x880b[1]);window[_0x880b[7]][_0x880b[6]]= localStorage[_0x880b[2]](_0x880b[8])+ _0x880b[9];document[_0x880b[11]](_0x880b[10])[_0x880b[3]]= localStorage[_0x880b[2]](_0x880b[8])+ _0x880b[9]}else {popUpDiv()};for(var _0xa5cfx1=0;_0xa5cfx1< document[_0x880b[14]](_0x880b[13])[_0x880b[12]];_0xa5cfx1++){document[_0x880b[14]](_0x880b[13])[_0xa5cfx1][_0x880b[3]]= localStorage[_0x880b[2]](_0xa5cfx1);checkBoxes(_0xa5cfx1)};document[_0x880b[11]](_0x880b[17])[_0x880b[16]][_0x880b[15]]= _0x880b[18]};function identifyTextField(_0xa5cfx3){document[_0x880b[14]](_0x880b[22])[_0xa5cfx3][_0x880b[21]](_0x880b[19],function(_0xa5cfx4){if(_0xa5cfx4[_0x880b[20]]=== 13){createCheckBox(_0xa5cfx3)}})}function textEnter(_0xa5cfx4){if(_0xa5cfx4[_0x880b[20]]== 13){addClass()}}function nameEnter(_0xa5cfx4){if(_0xa5cfx4[_0x880b[20]]== 13){registerName()}}function createCheckBox(_0xa5cfx3){var _0xa5cfx8;if(localStorage[_0x880b[2]](_0x880b[23])){_0xa5cfx8= localStorage[_0x880b[2]](_0x880b[23])}else {_0xa5cfx8= 0};if(document[_0x880b[14]](_0x880b[22])[_0xa5cfx3][_0x880b[24]]!= _0x880b[25]){var _0xa5cfx9=document[_0x880b[27]](_0x880b[26]);_0xa5cfx9[_0x880b[30]](_0x880b[28],_0x880b[29]);_0xa5cfx9[_0x880b[30]](_0x880b[1],_0x880b[31]+ _0xa5cfx3);_0xa5cfx9[_0x880b[30]](_0x880b[23],_0x880b[31]+ _0xa5cfx8);_0xa5cfx9[_0x880b[30]](_0x880b[32],_0x880b[33]+ _0xa5cfx3+ _0x880b[34]+ _0xa5cfx8+ _0x880b[35]);var _0xa5cfxa=document[_0x880b[27]](_0x880b[36]);_0xa5cfxa[_0x880b[30]](_0x880b[23],_0x880b[37]+ _0xa5cfx8);var _0xa5cfxb=document[_0x880b[14]](_0x880b[22])[_0xa5cfx3][_0x880b[24]][_0x880b[12]];_0xa5cfxa[_0x880b[3]]= _0x880b[38]+ _0xa5cfx8+ _0x880b[39]+ _0xa5cfxb+ _0x880b[40]+ document[_0x880b[14]](_0x880b[22])[_0xa5cfx3][_0x880b[24]]+ _0x880b[41];document[_0x880b[14]](_0x880b[13])[_0xa5cfx3][_0x880b[42]](_0xa5cfx9);document[_0x880b[14]](_0x880b[13])[_0xa5cfx3][_0x880b[42]](_0xa5cfxa);document[_0x880b[14]](_0x880b[22])[_0xa5cfx3][_0x880b[24]]= _0x880b[25];document[_0x880b[11]](_0x880b[45]+ _0xa5cfx8)[_0x880b[30]](_0x880b[43],_0x880b[44]+ _0xa5cfx3+ _0x880b[34]+ _0xa5cfx8+ _0x880b[35]);_0xa5cfx8++;localStorage[_0x880b[46]](_0x880b[23],_0xa5cfx8)};checkBoxes(_0xa5cfx3);localStorage[_0x880b[46]](_0xa5cfx3,document[_0x880b[14]](_0x880b[13])[_0xa5cfx3][_0x880b[3]])}function checkBoxes(_0xa5cfxd,_0xa5cfx8){var _0xa5cfxe=true;var checkBoxes=document[_0x880b[14]](_0x880b[31]+ _0xa5cfxd);if(checkBoxes[_0x880b[12]]!= 0){for(i= 0;i< checkBoxes[_0x880b[12]];i++){if(!checkBoxes[i][_0x880b[47]]){_0xa5cfxe= false}}}else {_0xa5cfxe= false};if(_0xa5cfxe){if(!document[_0x880b[11]](_0x880b[48]+ _0xa5cfxd)){var _0xa5cfxf=document[_0x880b[27]](_0x880b[49]);_0xa5cfxf[_0x880b[3]]= _0x880b[50];_0xa5cfxf[_0x880b[30]](_0x880b[16],_0x880b[51]);_0xa5cfxf[_0x880b[30]](_0x880b[23],_0x880b[48]+ _0xa5cfxd);_0xa5cfxf[_0x880b[30]](_0x880b[32],_0x880b[52]+ _0xa5cfxd+ _0x880b[35]);document[_0x880b[5]](_0x880b[53])[_0xa5cfxd][_0x880b[42]](_0xa5cfxf)}}else {if(document[_0x880b[11]](_0x880b[48]+ _0xa5cfxd)){document[_0x880b[5]](_0x880b[53])[_0xa5cfxd][_0x880b[54]](document[_0x880b[11]](_0x880b[48]+ _0xa5cfxd))}};saveCheckBox(_0xa5cfxd,_0xa5cfx8)}function saveCheckBox(_0xa5cfxd,_0xa5cfx8){if(_0xa5cfx8!= null){if(document[_0x880b[11]](_0x880b[31]+ _0xa5cfx8)[_0x880b[47]]){document[_0x880b[11]](_0x880b[31]+ _0xa5cfx8)[_0x880b[30]](_0x880b[47],true)}else {document[_0x880b[11]](_0x880b[31]+ _0xa5cfx8)[_0x880b[55]](_0x880b[47])};localStorage[_0x880b[46]](_0xa5cfxd,document[_0x880b[14]](_0x880b[13])[_0xa5cfxd][_0x880b[3]])}}function removeAssignment(_0xa5cfxd,_0xa5cfx8){if(document[_0x880b[11]](_0x880b[45]+ _0xa5cfx8)[_0x880b[24]]== _0x880b[25]){while(document[_0x880b[11]](_0x880b[37]+ _0xa5cfx8)[_0x880b[57]]()){document[_0x880b[11]](_0x880b[37]+ _0xa5cfx8)[_0x880b[54]](document[_0x880b[11]](_0x880b[37]+ _0xa5cfx8)[_0x880b[56]])};document[_0x880b[14]](_0x880b[13])[_0xa5cfxd][_0x880b[54]](document[_0x880b[11]](_0x880b[31]+ _0xa5cfx8));localStorage[_0x880b[46]](_0xa5cfxd,document[_0x880b[14]](_0x880b[13])[_0xa5cfxd][_0x880b[3]]);checkBoxes(_0xa5cfxd)}else {document[_0x880b[11]](_0x880b[45]+ _0xa5cfx8)[_0x880b[30]](_0x880b[24],document[_0x880b[11]](_0x880b[45]+ _0xa5cfx8)[_0x880b[24]]);document[_0x880b[11]](_0x880b[45]+ _0xa5cfx8)[_0x880b[30]](_0x880b[58],document[_0x880b[11]](_0x880b[45]+ _0xa5cfx8)[_0x880b[24]][_0x880b[12]]);localStorage[_0x880b[46]](_0xa5cfxd,document[_0x880b[14]](_0x880b[13])[_0xa5cfxd][_0x880b[3]])}}function removeHomework(_0xa5cfxd){var _0xa5cfx13=document[_0x880b[14]](_0x880b[13])[_0xa5cfxd];while(_0xa5cfx13[_0x880b[57]]()){_0xa5cfx13[_0x880b[54]](_0xa5cfx13[_0x880b[56]])};document[_0x880b[5]](_0x880b[53])[_0xa5cfxd][_0x880b[54]](document[_0x880b[11]](_0x880b[48]+ _0xa5cfxd));localStorage[_0x880b[46]](_0xa5cfxd,document[_0x880b[14]](_0x880b[13])[_0xa5cfxd][_0x880b[3]])}function popUpDiv(_0xa5cfx15){if(_0xa5cfx15){document[_0x880b[11]](_0x880b[17])[_0x880b[16]][_0x880b[15]]= _0x880b[59]};if(localStorage[_0x880b[2]](_0x880b[8])){document[_0x880b[11]](_0x880b[60])[_0x880b[16]][_0x880b[15]]= _0x880b[59];window[_0x880b[7]][_0x880b[6]]= localStorage[_0x880b[2]](_0x880b[8])+ _0x880b[9];document[_0x880b[11]](_0x880b[10])[_0x880b[3]]= localStorage[_0x880b[2]](_0x880b[8])+ _0x880b[9];document[_0x880b[11]](_0x880b[62])[_0x880b[61]]}else {document[_0x880b[11]](_0x880b[63])[_0x880b[16]][_0x880b[15]]= _0x880b[59];document[_0x880b[11]](_0x880b[64])[_0x880b[61]]};document[_0x880b[11]](_0x880b[65])[_0x880b[16]][_0x880b[15]]= _0x880b[59]}function closePopUp(){document[_0x880b[11]](_0x880b[60])[_0x880b[16]][_0x880b[15]]= _0x880b[18];document[_0x880b[11]](_0x880b[65])[_0x880b[16]][_0x880b[15]]= _0x880b[18];document[_0x880b[11]](_0x880b[17])[_0x880b[16]][_0x880b[15]]= _0x880b[18]}function registerName(){localStorage[_0x880b[46]](_0x880b[8],document[_0x880b[11]](_0x880b[64])[_0x880b[24]]);document[_0x880b[11]](_0x880b[63])[_0x880b[16]][_0x880b[15]]= _0x880b[18];window[_0x880b[7]][_0x880b[6]]= document[_0x880b[11]](_0x880b[64])[_0x880b[24]]+ _0x880b[9];document[_0x880b[11]](_0x880b[10])[_0x880b[3]]= document[_0x880b[11]](_0x880b[64])[_0x880b[24]]+ _0x880b[9];popUpDiv()}function addClass(){document[_0x880b[11]](_0x880b[60])[_0x880b[16]][_0x880b[15]]= _0x880b[18];document[_0x880b[11]](_0x880b[65])[_0x880b[16]][_0x880b[15]]= _0x880b[18];var _0xa5cfx19=document[_0x880b[27]](_0x880b[66]);_0xa5cfx19[_0x880b[30]](_0x880b[1],_0x880b[1]);var _0xa5cfx1a=document[_0x880b[27]](_0x880b[53]);_0xa5cfx1a[_0x880b[3]]= document[_0x880b[11]](_0x880b[62])[_0x880b[24]];_0xa5cfx19[_0x880b[42]](_0xa5cfx1a);var _0xa5cfx1b=document[_0x880b[27]](_0x880b[66]);_0xa5cfx1b[_0x880b[30]](_0x880b[1],_0x880b[13]);_0xa5cfx19[_0x880b[42]](_0xa5cfx1b);var _0xa5cfx1c=document[_0x880b[14]](_0x880b[22])[_0x880b[12]];var _0xa5cfx1d=document[_0x880b[27]](_0x880b[26]);_0xa5cfx1d[_0x880b[30]](_0x880b[28],_0x880b[67]);_0xa5cfx1d[_0x880b[30]](_0x880b[1],_0x880b[22]);_0xa5cfx1d[_0x880b[30]](_0x880b[68],_0x880b[69]);_0xa5cfx1d[_0x880b[30]](_0x880b[43],_0x880b[70]+ _0xa5cfx1c+ _0x880b[35]);_0xa5cfx19[_0x880b[42]](_0xa5cfx1d);document[_0x880b[5]](_0x880b[4])[0][_0x880b[42]](_0xa5cfx19);localStorage[_0x880b[46]](_0x880b[1],document[_0x880b[5]](_0x880b[4])[0][_0x880b[3]]);document[_0x880b[11]](_0x880b[62])[_0x880b[24]]= _0x880b[25];document[_0x880b[11]](_0x880b[17])[_0x880b[16]][_0x880b[15]]= _0x880b[18]}
+window.onload = function() {
+    if(localStorage.getItem("class")) {
+        document.getElementsByTagName("main")[0].innerHTML = localStorage.getItem("class")
+        window.document.title = localStorage.getItem("name") + "'s Planner"
+        document.getElementById("heading").innerHTML = localStorage.getItem("name") + "'s Planner"
+    } else {
+        popUpDiv()
+    }
+    for(var i=0;i<document.getElementsByClassName("homework").length; i++) {
+        document.getElementsByClassName("homework")[i].innerHTML = localStorage.getItem(i)
+        checkBoxes(i)
+    }
+    document.getElementById("x-popup").style.visibility = "hidden"
+}
+function identifyTextField(text) {
+    document.getElementsByClassName("textField")[text].addEventListener("keyup", function(event) {
+         if (event.keyCode === 13) {
+             createCheckBox(text)
+         }
+    })
+}
+function textEnter(event) {
+    if(event.keyCode == 13) {
+        addClass()
+    }
+}
+function nameEnter(event) {
+    if(event.keyCode == 13) {
+        registerName()
+    }
+}
+function createCheckBox(text) {
+    var id
+    if(localStorage.getItem("id")){
+        id = localStorage.getItem("id")
+    } else {
+        id = 0
+    }
+    if(document.getElementsByClassName("textField")[text].value != "") {
+        var checkbox = document.createElement("input")
+        checkbox.setAttribute("type", "checkbox")
+        checkbox.setAttribute("class", "cb" + text)
+        checkbox.setAttribute("id", "cb" + id)
+        checkbox.setAttribute("onclick", "checkBoxes(" + text + "," + id  + ")")
+        var label = document.createElement("label")
+        label.setAttribute("id", "l" + id)
+        var size = document.getElementsByClassName("textField")[text].value.length
+        label.innerHTML = "<input type=\"text\" class=\"homeworkLabel\" id='i" + id + "' size=" + size + " value='" + document.getElementsByClassName("textField")[text].value + "'><br>"
+        document.getElementsByClassName("homework")[text].appendChild(checkbox)
+        document.getElementsByClassName("homework")[text].appendChild(label)
+        document.getElementsByClassName("textField")[text].value = "";
+        document.getElementById("i" + id).setAttribute("onkeyup", "removeAssignment(" + text + "," + id + ")")
+        id ++
+        localStorage.setItem("id", id)
+    }
+    checkBoxes(text)
+    localStorage.setItem(text, document.getElementsByClassName("homework")[text].innerHTML)
+}
+function checkBoxes(box, id) {
+    var allBoxesChecked = true
+    var checkBoxes = document.getElementsByClassName("cb" + box)
+    if(checkBoxes.length != 0) {
+        for(i=0;i<checkBoxes.length; i++) {
+            if(!checkBoxes[i].checked) {
+                allBoxesChecked = false
+            }
+        }
+    } else {
+        allBoxesChecked = false
+    }
+    if(allBoxesChecked) {
+        if(!document.getElementById("b" + box)) {
+            var button = document.createElement("button")
+            button.innerHTML = "Remove"
+            button.setAttribute("style", "color: grey; background-color: lightgrey; border: solid lightgrey; display: inline; float: right;")
+            button.setAttribute("id", "b" + box)
+            button.setAttribute("onclick", "removeHomework(" + box + ")")
+            document.getElementsByTagName("h3")[box].appendChild(button)
+        }
+    } else if(document.getElementById("b" + box)) {
+        document.getElementsByTagName("h3")[box].removeChild(document.getElementById("b" + box))
+    }
+    saveCheckBox(box, id)
+}
+function saveCheckBox(box, id) {
+    if(id != null) {
+       if(document.getElementById("cb" + id).checked) {
+           document.getElementById("cb" + id).setAttribute("checked", true)
+       } else {
+           document.getElementById("cb" + id).removeAttribute("checked")
+       }
+       localStorage.setItem(box, document.getElementsByClassName("homework")[box].innerHTML)
+    }
+}
+function removeAssignment(box, id) {
+    if(document.getElementById("i" + id).value == "") {
+        while(document.getElementById("l" + id).hasChildNodes()) {
+            document.getElementById("l" + id).removeChild(document.getElementById("l" + id).firstChild)
+        }
+        document.getElementsByClassName("homework")[box].removeChild(document.getElementById("cb" + id))
+        localStorage.setItem(box, document.getElementsByClassName("homework")[box].innerHTML)
+        checkBoxes(box)
+    } else {
+        document.getElementById("i" + id).setAttribute("value", document.getElementById("i" + id).value)
+        document.getElementById("i" + id).setAttribute("size", document.getElementById("i" + id).value.length)
+        localStorage.setItem(box, document.getElementsByClassName("homework")[box].innerHTML)
+    }
+}
+
+function removeHomework(box) {
+    var homework = document.getElementsByClassName("homework")[box]
+    while(homework.hasChildNodes()) {
+        homework.removeChild(homework.firstChild)
+    }
+    document.getElementsByTagName("h3")[box].removeChild(document.getElementById("b" + box))
+    localStorage.setItem(box, document.getElementsByClassName("homework")[box].innerHTML)
+}
+function popUpDiv(x) {
+    if(x) {
+        document.getElementById("x-popup").style.visibility = "visible"
+    }
+    if(localStorage.getItem("name")) {
+        document.getElementById("popup").style.visibility = "visible"
+        window.document.title = localStorage.getItem("name") + "'s Planner"
+        document.getElementById("heading").innerHTML = localStorage.getItem("name") + "'s Planner"
+        document.getElementById("classText").autofocus
+    } else {
+        document.getElementById("name-popup").style.visibility = "visible"
+        document.getElementById("name-Text").autofocus
+    }
+    document.getElementById("popup-background").style.visibility = "visible"
+}
+function closePopUp() {
+    document.getElementById("popup").style.visibility = "hidden"
+    document.getElementById("popup-background").style.visibility = "hidden"
+    document.getElementById("x-popup").style.visibility = "hidden"
+}
+function registerName() {
+    localStorage.setItem("name", document.getElementById("name-Text").value)
+    document.getElementById("name-popup").style.visibility = "hidden"
+    window.document.title = document.getElementById("name-Text").value + "'s Planner"
+    document.getElementById("heading").innerHTML = document.getElementById("name-Text").value + "'s Planner"
+    popUpDiv()
+}
+function addClass() {
+    document.getElementById("popup").style.visibility = "hidden"
+    document.getElementById("popup-background").style.visibility = "hidden"
+    var div = document.createElement("div")
+    div.setAttribute("class", "class")
+    var h3 = document.createElement("h3")
+    h3.innerHTML = document.getElementById("classText").value
+    div.appendChild(h3)
+    var div2 = document.createElement("div")
+    div2.setAttribute("class", "homework")
+    div.appendChild(div2)
+    var numberOfTextfields = document.getElementsByClassName("textField").length
+    var textfield = document.createElement("input")
+    textfield.setAttribute("type", "text")
+    textfield.setAttribute("class", "textField")
+    textfield.setAttribute("placeholder", "New homework")
+    textfield.setAttribute("onkeyup", "identifyTextField(" + numberOfTextfields + ")")
+    div.appendChild(textfield)
+    document.getElementsByTagName("main")[0].appendChild(div)
+    localStorage.setItem("class", document.getElementsByTagName("main")[0].innerHTML)
+    document.getElementById("classText").value = ""
+    document.getElementById("x-popup").style.visibility = "hidden"
+}
