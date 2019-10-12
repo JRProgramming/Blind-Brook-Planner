6fe001bbf8c30f23566a839dbf4a64cec5569171
Johnny Ramirez <JRProgramming@users.noreply.github.com>
2019-10-11 22:24:57 -0400

    Fixes bug where width wouldn't extend full way with some class when "Remove" was activated

--- script.js
+++ script.js
@@ -125,6 +125,7 @@ function checkBoxes(box, id, loop) {
                     document.getElementsByClassName("titleDiv")[box].appendChild(button)
                     if(document.getElementById("b" + box).offsetTop > 25) {
                          document.getElementsByClassName("class")[box].style.maxWidth = (Number(document.getElementsByClassName("class")[box].style.maxWidth.slice(0, -2)) + Number(button.style.width.slice(0, -2)) + 11) + "px"
+                        document.getElementsByClassName("class")[box].style.width = (Number(document.getElementsByClassName("class")[box].style.maxWidth.slice(0, -2)) + Number(button.style.width.slice(0, -2)) + 11) + "px"
                     }
                 }
             } else if(document.getElementById("b" + box)) {
@@ -158,6 +159,7 @@ function checkBoxes(box, id, loop) {
                 document.getElementsByClassName("titleDiv")[box].appendChild(button)
                 if(document.getElementById("b" + box).offsetTop > 25) {
                      document.getElementsByClassName("class")[box].style.maxWidth = (Number(document.getElementsByClassName("class")[box].style.maxWidth.slice(0, -2)) + Number(button.style.width.slice(0, -2)) + 11) + "px"
+                    document.getElementsByClassName("class")[box].style.width = (Number(document.getElementsByClassName("class")[box].style.maxWidth.slice(0, -2)) + Number(button.style.width.slice(0, -2)) + 11) + "px"
                 }
             }
         } else if(document.getElementById("b" + box)) {
